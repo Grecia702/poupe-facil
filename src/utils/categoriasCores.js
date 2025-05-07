e85c1792198d496728740de324f5839bea50aee1
Gabriel Grecia <gabriel.lena81@gmail.com>
2025-05-07 01:47:30 -0400

    adiciona objeto de par key/value de cores

---
+++ src/utils/categoriasCores.js
@@ -0,0 +1,10 @@
+export const categoriaCores = {
+    Contas: "rgb(160, 48, 44)",
+    Alimentação: "rgb(204, 118, 38)",
+    Carro: "rgb(57, 184, 74)",
+    Internet: "rgb(64, 155, 230)",
+    Lazer: "rgb(114, 13, 109)",
+    Educação: "rgb(68, 59, 90)",
+    Compras: "rgb(148, 137, 37)",
+    Outros: "rgb(83, 87, 83)",
+};
