2eb6e9820d9be57e068cf2d2333a3db89bb11728
Gabriel Grecia <gabriel.lena81@gmail.com>
2025-05-07 01:03:52 -0400

    feat: adiciona hook de infinity query

---
+++ src/hooks/usePosts.js
@@ -0,0 +1,34 @@
+import { useInfiniteQuery } from '@tanstack/react-query'
+import api from '@context/axiosInstance'
+
+const fetchPosts = async ({ pageParam = 1, queryKey }) => {
+    const [, { tipo, natureza, orderBy, orderDirection }] = queryKey
+    try {
+        const response = await api.get('/profile/transaction', {
+            params: {
+                ...(natureza && { natureza }),
+                ...(tipo && { tipo }),
+                orderBy,
+                orderDirection,
+                page: pageParam,
+                limit: 12
+            },
+        })
+
+        const { data, meta } = response.data;
+        return { data, meta };
+    } catch (error) {
+        console.error('Erro ao buscar posts:', error);
+        throw error;
+    }
+}
+
+export const usePosts = (filters) => {
+    return useInfiniteQuery({
+        queryKey: ['posts', filters],
+        queryFn: fetchPosts,
+        getNextPageParam: (lastPage) => {
+            return lastPage?.meta?.hasNextPage ? lastPage?.meta?.page + 1 : undefined;
+        },
+    });
+};
