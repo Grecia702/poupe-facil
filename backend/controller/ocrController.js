278a90013fe92321a5246a6794e85d7f6604ad55
Gabriel Grecia <gabriel.lena81@gmail.com>
2025-06-10 16:40:28 -0400

    feat: adiciona o cloud vision para uso de OCR

--- backend/controller/ocrController.js
+++ backend/controller/ocrController.js
@@ -1,20 +1,29 @@
-const Tesseract = require('tesseract.js');
 const path = require('path');
 const fs = require('fs');
-const cv = require('opencv4nodejs');
+const vision = require('@google-cloud/vision');
 const { promptOCR } = require("../services/openaiService");
+require('dotenv').config();
+
+const client = new vision.ImageAnnotatorClient();
 
 const processImage = async (req, res) => {
     if (!req.file) {
         return res.status(400).json({ error: 'Nenhuma imagem enviada' });
     }
+
     try {
         const imagePath = path.resolve(req.file.path);
+        const [result] = await client.textDetection(imagePath);
+        const detections = result.textAnnotations;
 
-        const { data: { text } } = await Tesseract.recognize(imagePath, 'por');
-        console.log(text)
+        if (!detections || detections.length === 0) {
+            fs.unlinkSync(imagePath);
+            return res.status(200).json({ text: '', message: 'Nenhum texto encontrado' });
+        }
+        const text = detections[0].description;
+        console.log(text);
         fs.unlinkSync(imagePath);
-        const data = await promptOCR(text)
+        const data = await promptOCR(text);
         return res.status(200).json(data[0]);
     } catch (error) {
         return res.status(500).json({ error: 'Erro ao processar OCR', details: error.message });
