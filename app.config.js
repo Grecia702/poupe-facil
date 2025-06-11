3be76ec0131315b6439292ad51d903affcfff24d
Gabriel Grecia <gabriel.lena81@gmail.com>
2025-06-10 20:13:23 -0400

    fix: corrige env vars

---
+++ app.config.js
@@ -0,0 +1,23 @@
+import 'dotenv/config';
+
+export default () => ({
+    expo: {
+        name: 'poupefacil',
+        slug: 'poupefacil',
+        version: '1.0.0',
+        orientation: 'portrait',
+        extra: {
+            API_URL: process.env.API_URL || "http://192.168.100.211/api",
+            eas: {
+                projectId: process.env.PROJECT_ID,
+            },
+        },
+        android: {
+            package: 'com.anonymous.poupefacilapp',
+        },
+        assetBundlePatterns: [
+            '**/*',
+            '!backend/**/*',
+        ],
+    },
+});
