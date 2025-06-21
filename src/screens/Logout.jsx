18af8f5219f7f8df3673f8de4e0d3dcc926f9f3e
Gabriel Grecia <gabriel.lena81@gmail.com>
2025-06-21 02:20:31 -0400

    feat(UI): adiciona toast message em logout

--- src/screens/Logout.jsx
+++ src/screens/Logout.jsx
@@ -2,24 +2,40 @@ import { View } from 'react-native'
 import { useAuth } from '@context/authContext';
 import { useNavigation } from '@react-navigation/native'
 import DangerModal from '@components/dangerModal';
+import { useToast } from 'react-native-toast-notifications';
 
 const Logout = ({ isOpen, setIsOpen }) => {
     const navigation = useNavigation();
+    const toast = useToast();
     const { logoutMutation } = useAuth()
     const handleLogout = async () => {
         setIsOpen(false)
         logoutMutation.mutate(null, {
             onSuccess: () => {
+                toastSuccess();
                 navigation.reset({
                     index: 0,
                     routes: [{ name: 'login' }],
                 });
             },
-            onError: () => {
-                setMessage('Falha no logout, tente novamente');
+            onError: (error) => {
+                ToastError(`Erro ao fazer logout: ${error}`)
             },
         });
     }
+    const toastSuccess = () => {
+        toast.show('Logout feito com sucesso', {
+            type: 'success',
+            duration: 3000,
+        })
+    }
+    const ToastError = (message) => {
+        toast.show(`${message}`, {
+            type: 'error',
+            duration: 3000,
+        })
+    }
+
     return (
         <View style={{ flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
             <DangerModal
