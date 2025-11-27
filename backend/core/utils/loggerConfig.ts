cf504887549769df8daa282ede9e70b2b103e298
Gabriel Grecia <gabriel.lena81@gmail.com>
2025-11-27 08:45:50 -0400

    feat: adiciona middleware de logging

--- backend/core/utils/loggerConfig.ts
+++ backend/core/utils/loggerConfig.ts
@@ -1,26 +1,36 @@
 import winston from 'winston';
-import type { TransformableInfo } from 'logform'
+import type { TransformableInfo } from 'logform';
 import { format as dateFormat } from 'date-fns';
 
 const logger = winston.createLogger({
     level: 'info',
     format: winston.format.combine(
+        winston.format.timestamp(),
         winston.format.printf((info: TransformableInfo) => {
-            const { level, message } = info;
-            const timestamp = dateFormat(new Date(), 'yyyy-MM-dd HH:mm:ss');
-            return `${timestamp} ${level}: ${message}`;
+            const { level, timestamp, ...meta } = info;
+            const formattedTime = dateFormat(new Date(timestamp as string), 'yyyy-MM-dd HH:mm:ss');
+            const message = JSON.stringify(meta);
+            return `${formattedTime} ${level}: ${message}`;
         })
     ),
     transports: [
         new winston.transports.Console({
             format: winston.format.combine(
-                winston.format.colorize(),
-                winston.format.simple()
+                winston.format.colorize({ level: true }),
+                winston.format.printf((info: TransformableInfo) => {
+                    const { level, timestamp, ...meta } = info;
+                    const formattedTime = dateFormat(new Date(timestamp as string), 'yyyy-MM-dd HH:mm:ss');
+                    return `${formattedTime} ${level}: ${JSON.stringify(meta)}`;
+                })
             )
         }),
-        new winston.transports.File({ filename: 'logs/app.log' })
+
+        new winston.transports.File({
+            filename: 'logs/app.log',
+            maxsize: 5 * 1024 * 1024,
+            maxFiles: 5,
+        })
     ]
 });
-
 
 export default logger;
